909f48a45bc4eae09d3674972cb7804b5720dcd8
99Kies <1290017556@qq.com>
2021-06-29 18:25:30 +0800

    update datamin api return

--- eternity_backend_server/blueprints/datamin/detail_function/bishijie_parse.py
+++ eternity_backend_server/blueprints/datamin/detail_function/bishijie_parse.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup
 from lxml import etree
 import json
+import re
 
 # xpath需要定期更换，否则数据抓取不全
 
-def  bishijie_info_parse(parse_str:str = ''):
+def  bishijie_info_parse(URL, parse_str:str = ''):
     """
     传入一个待解析的字符串
     """
@@ -15,10 +16,13 @@ def  bishijie_info_parse(parse_str:str = ''):
     for info in info_list:
         title = info.find('h3').text.replace('\n','').replace(' ','')
         content = info.find('div',class_='h63').text.replace('\n','').replace(' ','')
+
+        url = URL + info.find('a')["href"]
         look_count_true = info.find('div',class_='bull').text.replace('\n','').replace(' ','')
         look_count_flase = info.find('div',class_='bear').text.replace('\n','').replace(' ','')
-        result_info_list.append({"title":title,
-                                 "content":content,
+        result_info_list.append({"Title":title,
+                                 "Description":content,
+                                 "Url": url,
                                  "look_count_true":look_count_true,
                                  "look_count_flase":look_count_flase})
     result_info = {
